66dc2cc6d723b0365a5a401faf61296dc2012dff
ATBSPB <3634914408@qq.com>
2026-06-09 03:11:19 +0800

    fix: 移除版权栏滚动渐显逻辑，更新 README

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,38 +1,11 @@
 <template>
-  <footer :style="{ opacity: footerOpacity }">
+  <footer>
     Copyright &copy; {{ currentYear }} ATBSPB. All rights reserved.
   </footer>
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref } from 'vue'
 
 const currentYear = ref(new Date().getFullYear())
-const footerOpacity = ref(0)
-
-const FADE_THRESHOLD = 300
-
-function handleScroll() {
-  const scrollTop = window.scrollY || document.documentElement.scrollTop
-  const windowHeight = window.innerHeight
-  const documentHeight = document.documentElement.scrollHeight
-  const distanceFromBottom = documentHeight - (scrollTop + windowHeight)
-
-  if (distanceFromBottom <= 0) {
-    footerOpacity.value = 1
-  } else if (distanceFromBottom >= FADE_THRESHOLD) {
-    footerOpacity.value = 0
-  } else {
-    footerOpacity.value = 1 - distanceFromBottom / FADE_THRESHOLD
-  }
-}
-
-onMounted(() => {
-  window.addEventListener('scroll', handleScroll, { passive: true })
-  handleScroll()
-})
-
-onUnmounted(() => {
-  window.removeEventListener('scroll', handleScroll)
-})
 </script>
